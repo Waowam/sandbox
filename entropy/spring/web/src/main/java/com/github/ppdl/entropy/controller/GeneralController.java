4fbfc0afc52bcc0e011ca3617f66bcc9f3028a6f
Laurent BIZE <bize.laurent@gmail.com>
2013-12-26 23:30:04 +0100

    ADD form1 mapping

--- entropy/spring/web/src/main/java/com/github/ppdl/entropy/controller/GeneralController.java
+++ entropy/spring/web/src/main/java/com/github/ppdl/entropy/controller/GeneralController.java
@@ -1,10 +1,15 @@
 package com.github.ppdl.entropy.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class GeneralController {
 
-	
+	@RequestMapping(value="/form1", method=RequestMethod.GET)
+	public String doForm1() {
+		return "form1";
+	}
 	
 }
