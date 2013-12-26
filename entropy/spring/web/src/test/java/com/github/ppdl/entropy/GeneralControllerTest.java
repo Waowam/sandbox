dba7d1605191854d8ed71c3bb0d6e1409822c3b0
Laurent BIZE <bize.laurent@gmail.com>
2013-12-26 23:34:35 +0100

    ADD empty test

---
+++ entropy/spring/web/src/test/java/com/github/ppdl/entropy/GeneralControllerTest.java
@@ -0,0 +1,14 @@
+/**
+ * 
+ */
+package com.github.ppdl.entropy;
+
+/**
+ * <p>Tests for {@link GeneralController}</p>
+ * 
+ * @author Laurent BIZE
+ *
+ */
+public class GeneralControllerTest {
+
+}
